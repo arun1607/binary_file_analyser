78c816702e62e441abb71c5ebcda0b539a0070f9
Amit Shrivastava <amit.shrivastava@idmworks.com>
2018-11-25 21:17:41 +0530

    Added test cases

--- src/main/java/com/app/learning/core/ArchitectureType.java
+++ src/main/java/com/app/learning/core/ArchitectureType.java
@@ -4,15 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ArchitectureType {
-    EM_NONE(1, "No machine"),
-    EM_M32(2, "AT&T WE 32100"),
-    EM_SPARC(3, "SPARC"),
-    EM_386(4, "Intel 80386"),
-    EM_68K(5, "Motorola 68000"),
-    EM_88K(6, "Motorola 88000"),
-    EM_IAMCU(7, "Intel MCU"),
-    EM_860(8, "Intel 80860"),
-    EM_MIPS(9, "MIPS I Architecture")
+    EM_NONE(0, "No machine"),
+    EM_M32(1, "AT&T WE 32100"),
+    EM_SPARC(2, "SPARC"),
+    EM_386(3, "Intel 80386"),
+    EM_68K(4, "Motorola 68000"),
+    EM_88K(5, "Motorola 88000"),
+    EM_IAMCU(6, "Intel MCU"),
+    EM_860(7, "Intel 80860"),
+    EM_MIPS(8, "MIPS I Architecture"),
+    EM_S370(9, "IBM System/370 Processor"),
+    EM_MIPS_RS3_LE(10, "MIPS RS3000 Little-endian"),
+    EM_PARISC(15, "Hewlett-Packard PA-RISC"),
+    EM_VPP500(17, "Fujitsu VPP500"),
+    EM_SPARC32PLUS(18, "Enhanced instruction set SPARC"),
+    EM_960(19, "Intel 80960"),
+    EM_PPC(20, "PowerPC"),
+    EM_PPC64(21, "64-bit PowerPC"),
+    EM_S390(22, "IBM System/390 Processor"),
+    EM_SPU(23, "IBM SPU/SPC"),
+    EM_V800(36, "NEC V800"),
+    EM_FR20(37, "Fujitsu FR20"),
+    EM_RH32(38, "TRW RH-32"),
+    EM_RCE(39, "Motorola RCE"),
+    EM_ARM(40, "ARM 32-bit architecture (AARCH32)"),
+    EM_X86_64(62, "AMD x86-64 architecture")
     ;
 
 
--- src/test/java/com/app/learning/core/ElfParserTest.java
+++ src/test/java/com/app/learning/core/ElfParserTest.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.nio.file.Files;
 
 import static com.app.learning.core.Constants.CLASS_32;
+import static com.app.learning.core.Constants.CLASS_64;
 
 public class ElfParserTest {
 
@@ -25,5 +26,21 @@ public class ElfParserTest {
         final ElfParser elfParser = new ElfParser(Files.readAllBytes(input.toPath()));
         elfParser.parse();
         Assert.assertEquals(CLASS_32,elfParser.getFileClass());
+    }
+
+    @Test
+    public void parseElfFile_1() throws Exception {
+        File input = new File("src/test/resources/android_arm_libncurses");
+        final ElfParser elfParser = new ElfParser(Files.readAllBytes(input.toPath()));
+        elfParser.parse();
+        Assert.assertEquals(CLASS_32, elfParser.getFileClass());
+    }
+
+    @Test
+    public void parseElfFile_2() throws Exception {
+        File input = new File("src/test/resources/linux_amd64_bindash");
+        final ElfParser elfParser = new ElfParser(Files.readAllBytes(input.toPath()));
+        elfParser.parse();
+        Assert.assertEquals(CLASS_64, elfParser.getFileClass());
     }
 }
